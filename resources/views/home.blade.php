f820794314fd73a75cf86b1476e6bae8ed08ef7d
syifasitikhumairah <syifasiti158@gmail.com>
2021-10-23 13:37:37 +0700

    tes-admin

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,35 @@
 @extends('layouts.admin')
+
 @section('header')
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col -sm-12">
-                <h1 class="m-0">Home</h1>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="conten-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-md-12">
+                    <h1 class="m-0">Home</h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
