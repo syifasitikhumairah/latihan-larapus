aed2e02b96e0fe8b132c8f297afd82b489d3b0e3
syifasitikhumairah <syifasiti158@gmail.com>
2021-11-13 11:35:19 +0700

    Book

--- resources/views/layouts/patrials/sidebar.blade.php
+++ resources/views/layouts/patrials/sidebar.blade.php
@@ -48,15 +48,15 @@
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
+                <a href="./author" class="nav-link active">
                     <i class="far fa-circle nav-icon"></i>
-                    <p>Dashboard v1</p>
+                    <p>Data Penulis</p>
                 </a>
                 </li>
                 <li class="nav-item">
-                <a href="./index2.html" class="nav-link">
+                <a href="./books" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
-                    <p>Dashboard v2</p>
+                    <p>Data Buku</p>
                 </a>
                 </li>
                 <li class="nav-item">
